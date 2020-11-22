4ab162060f762b57827dd35840bee6541c547e8a
Akshay Nair <phenax5@gmail.com>
2020-11-22 17:05:06 +0530

    Adds help instructions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# sudo pip install -U setuptools wheel
+# sudo pip install -r misc/requirements/requirements-pyqt.txt --only-binary PyQt5,PyQtWebEngine
+# sudo pip install -r requirements.txt
+# yay -S python-pypeg2 python-pygments python-markupsafe python-jinja python-attrs python-pyqt5 python-pyqt5-sip python-pyqtwebengine
+
 build() {
     python scripts/asciidoc2html.py && \
     a2x -f manpage doc/qutebrowser.1.asciidoc && \
@@ -15,3 +20,9 @@ cleanup() {
 }
 
 build && package && cleanup;
+
+
+### REMOVE
+# sudo pip uninstall pyqt5 pyqtwebengine pypeg2 attrs markupsafe pyqt5-sip pygments
+# sudo pip uninstall qutebrowser
+
